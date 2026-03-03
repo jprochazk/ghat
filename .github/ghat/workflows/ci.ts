e4262c369163c865a11364b1e118e7e191f5d1da
jprochazk <pjanv42@gmail.com>
2026-03-04 00:16:32 +0100

    run ci on all targets
    note that i'm intentionally not running `ghat generate` here, to
    exercise the check workflow

--- .github/ghat/workflows/ci.ts
+++ .github/ghat/workflows/ci.ts
@@ -11,12 +11,23 @@ workflow("CI", {
 
   jobs(ctx) {
     ctx.job("Test", {
-      runs_on: "ubuntu-latest",
+      strategy: matrix({
+        include: [
+          { runner: "ubuntu-24.04", target: "" },
+          { runner: "ubuntu-24.04-arm", target: "" },
+          { runner: "macos-26", target: "" },
+          { runner: "windows-2025", target: "x86_64-pc-windows-gnu" },
+        ]
+      }),
 
-      steps() {
+      runs_on: (ctx) => ctx.matrix.runner,
+
+      steps(ctx) {
         uses("actions/checkout")
         uses("Swatinem/rust-cache")
-        uses("dtolnay/rust-toolchain", { with: { toolchain: "stable" } })
+        uses("dtolnay/rust-toolchain", {
+          with: { toolchain: "stable", targets: ctx.matrix.target }
+        })
         run("cargo test")
       }
     })
